b86c81ea36402e26708fbc5ee8d37b3706c7e576
edvanbarbosa <edvabarbosa279@gmail.com>
2023-02-26 18:50:29 -0300

    CSS improvement, html rectifications and js creation

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const lists = document.querySelector('.b_ex')
+const option = document.querySelectorAll('.options')
+
+lists.addEventListener('click',(e)=>{
+    const obj = e.target.className
+    const o = option.map(
+        
+    )
+
+})
