45216d2fb817ece88e889770fbd985c20df1f5ac
edvanbarbosa <edvabarbosa279@gmail.com>
2023-02-26 22:33:03 -0300

    edit js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-const lists = document.querySelector('.b_ex')
-const option = document.querySelectorAll('.options')
+const lists = document.querySelectorAll('.b_ex')
+const tt = document.querySelectorAll('.options')
 
-lists.addEventListener('click',(e)=>{
-    const obj = e.target.className
-    const o = option.map(
+console.log(lists)
+
+lists.forEach(o => {
+    o.addEventListener('click',(e)=>{
+
         
-    )
+        const ob = e.target.className
+        const obj = ob.replace("b_ex",'')
+        let obj_test = document.querySelector("ul."+obj).style.display
+        obj_test =  obj_test == "flex"?"none":"flex"
+        document.querySelector("ul."+obj).style.display = obj_test
 
-})
+        tt.forEach(oe => {
+            if(oe.className != ob.replace("b_ex","options") ){
+                oe.style.display = "none"
+            }
+        });
+        
+    
+    
+    })
+});
+
