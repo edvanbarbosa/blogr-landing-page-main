39fe49877314018f70fbd1326ff46f47214a5afd
edvanbarbosa <edvabarbosa279@gmail.com>
2023-03-06 11:06:07 -0300

    ...

--- src/main.js
+++ src/main.js
@@ -7,17 +7,18 @@ lists.forEach(o => {
     o.addEventListener('click',(e)=>{
 
         
-        const ob = e.target.className
-        const obj = ob.replace("b_ex",'')
-        let obj_test = document.querySelector("ul."+obj).style.display
-        obj_test =  obj_test == "flex"?"none":"flex"
-        document.querySelector("ul."+obj).style.display = obj_test
+        const element = e.target.className.replace('b_ex', '')
 
-        tt.forEach(oe => {
-            if(oe.className != ob.replace("b_ex","options") ){
-                oe.style.display = "none"
-            }
-        });
+
+        let test = e.target.className == 'timg' ?true:false
+
+        if(test == false){
+            let eu = document.querySelector('ul.'+element)
+            eu.style.display = "flex"
+
+            console.log(eu,test)
+        }
+         
         
     
     
