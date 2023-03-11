73bae007294f6d0c3de2182e18feca39a7805ba7
edvanbarbosa <edvabarbosa279@gmail.com>
2023-03-11 13:52:46 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 const lists = document.querySelectorAll('.b_ex')
 const tt = document.querySelectorAll('.options')
 
-console.log(lists)
 
 lists.forEach(o => {
-    o.addEventListener('click',(e)=>{
+    o.addEventListener('mouseover',(e)=>{
 
         
         const element = e.target.className.replace('b_ex', '')
 
 
-        let test = e.target.className == 'timg' ?true:false
+        let test = e.target.className
 
-        if(test == false){
+        if(test != 'timg'){
             let eu = document.querySelector('ul.'+element)
             eu.style.display = "flex"
 
             console.log(eu,test)
+        }
+        else{
+            console.log('eu tava certo')
         }
          
         
