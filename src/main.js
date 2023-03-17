176611213a1877594f47df800fa133fcb2c20b33
edvanbarbosa <edvabarbosa279@gmail.com>
2023-03-17 16:08:17 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,36 @@ const lists = document.querySelectorAll('.b_ex')
 const tt = document.querySelectorAll('.options')
 
 
+const active= (e)=>{
+            
+    const element = e.target.className.replace('b_ex', '')
+
+
+    let test = e.target.className
+
+    if(test != 'timg'){
+        let eu = document.querySelector('ul.'+element)
+        eu.style.display = "flex"
+
+        console.log(eu,test)
+    }
+    else{
+        console.log('eu tava certo')
+    }
+     
+    
+
+}
+
+let evente = 'mouseover'
+
 lists.forEach(o => {
-    o.addEventListener('mouseover',(e)=>{
+    o.addEventListener(evente,(e)=>{
+        active(e)
+
+    
+    })
+    o.addEventListener('mouseout',(e)=>{
 
         
         const element = e.target.className.replace('b_ex', '')
@@ -13,7 +41,7 @@ lists.forEach(o => {
 
         if(test != 'timg'){
             let eu = document.querySelector('ul.'+element)
-            eu.style.display = "flex"
+            eu.style.display = "none"
 
             console.log(eu,test)
         }
@@ -25,5 +53,6 @@ lists.forEach(o => {
     
     
     })
+    
 });
 
