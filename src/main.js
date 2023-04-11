be3a4a0dc650942ec8700fef931dc6b9cc7dbc8c
Edvan Barbosa <114129267+edvanbarbosa@users.noreply.github.com>
2023-04-11 09:09:21 -0300

    cc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const lists = document.querySelectorAll('.b_ex')
 const tt = document.querySelectorAll('.options')
+const options = document.querySelectorAll('.t_options')
 
 
 const active= (e)=>{
@@ -13,7 +14,7 @@ const active= (e)=>{
         let eu = document.querySelector('ul.'+element)
         eu.style.display = "flex"
 
-        console.log(eu,test)
+        console.log(test)
     }
     else{
         console.log('eu tava certo')
@@ -23,36 +24,17 @@ const active= (e)=>{
 
 }
 
-let evente = 'mouseover'
-
 lists.forEach(o => {
-    o.addEventListener(evente,(e)=>{
+    o.addEventListener('mouseover',(e)=>{
         active(e)
 
     
     })
-    o.addEventListener('mouseout',(e)=>{
-
-        
+    o.addEventListener('mouseout',(b)=>{
         const element = e.target.className.replace('b_ex', '')
-
-
         let test = e.target.className
 
-        if(test != 'timg'){
-            let eu = document.querySelector('ul.'+element)
-            eu.style.display = "none"
-
-            console.log(eu,test)
-        }
-        else{
-            console.log('eu tava certo')
-        }
-         
         
-    
-    
     })
     
 });
-
