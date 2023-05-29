8724c5b3a71af9c99d29d66221f487ad38357615
Edvan Barbosa <114129267+edvanbarbosa@users.noreply.github.com>
2023-05-29 15:54:14 -0300

    conserto

--- src/main.js
+++ src/main.js
@@ -1,64 +1,27 @@
-const lists = document.querySelectorAll('.b_ex')
-const tt = document.querySelectorAll('.options')
-const options = document.querySelectorAll('.t_options')
+const t_op = document.querySelectorAll('.t_options')
 
 
-const active= (e)=>{
-            
-    const element = e.target.className.replace('b_ex', '')
+t_op.forEach(e =>{
+    e.addEventListener('mouseover',()=>{
+        const cl = e.className.replace('t_options ','')
 
+        const options = document.querySelector("ul."+cl)
+        options.style.display = 'flex'
 
-    let test = e.target.className
-
-    if(test != 'timg'){
-        let eu = document.querySelector('ul.'+element)
-        eu.style.display = "flex"
-
-        console.log(test)
-    }
-    else{
-        console.log('eu tava certo')
-    }
-     
-    
-
-}
-
-ttt = false
-
-tt.forEach((e)=>{
-    e.addEventListener('mouseover',(o)=>{
-        ttt = true
+        const butimg = document.querySelector('button.'+cl+">img")
+        butimg.style.rotate = '180deg'
+      
         
     })
+    e.addEventListener('mouseout',()=>{
+        const cl = e.className.replace('t_options ','')
 
-    e.addEventListener('mouseout',(o)=>{
-        ttt = false 
+        const options = document.querySelector("ul."+cl)
+        options.style.display = 'none'
+
+        const butimg = document.querySelector('button.'+cl+">img")
+        butimg.style.rotate = '0deg'
+      
+        
     })
 })
-
-lists.forEach(o => {
-    o.addEventListener('mouseover',(e)=>{
-        active(e)
-
-    
-    })
-    o.addEventListener('mouseout',(b)=>{
-        const element = b.target.className.replace('b_ex', '')
-        let test = b.target.className
-
-        
-        
-        setTimeout(()=>{
-            if (ttt != true){
-                let eu = document.querySelector('ul.'+element)
-                eu.style.display = "none"
-            }
-        },300)
-       
-
-
-        
-    })
-    
-});
